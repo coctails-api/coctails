18608a9f72f4f36cd318ea544e759cedbb851add
Mateusz Żak <matzakoo@gmail.com>
2022-10-19 21:45:55 +0200

    Initial commit

---
+++ src/main/java/com/massage/MassageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.massage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/massage/MassageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
