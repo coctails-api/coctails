8fe59a0ca5f2759937a19f021f83772f04d47f26
Mateusz Żak <matzakoo@gmail.com>
2022-10-22 17:12:00 +0200

    Swagger2 configured at the moment without security http://localhost:????/swagger-ui/

--- src/main/java/com/massage/MassageSystemApplication.java
+++ src/main/java/com/massage/MassageSystemApplication.java
@@ -1,9 +1,13 @@
 package com.massage;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class MassageSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MassageSystemApplication.class, args);
