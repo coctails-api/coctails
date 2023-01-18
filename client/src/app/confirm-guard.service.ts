5472a807714db38861eaf9f4810608fa423c2005
Mateusz Żak <matzakoo@gmail.com>
2023-01-18 11:12:20 +0100

    front-end design refactor pt.4

---
+++ client/src/app/confirm-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {AuthService} from "./service/auth.service";
+import {AuthenticationService} from "./service/authentication.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmGuard implements CanActivate {
+
+  constructor(private authService: AuthenticationService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (!this.authService.isUserLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/index']);
+      return false;
+    }
+  }
+}
