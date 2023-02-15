8ab6ed7b5045290aa90fc521584d64bc4da6bd3d
Mateusz Żak <matzakoo@gmail.com>
2023-02-15 18:48:32 +0100

    disabling admin for admin

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {IndexComponent} from "./aaapages/index/index.component";
 import {ConfirmEmailComponent} from "./aaapages/confirm-email/confirm-email.component";
 import {AdminGuard} from "./admin-guard.service";
 import {NewPasswordComponent} from "./aaapages/new-password/new-password.component";
+import {AdminIndexComponent} from "./aaapages/admin/aaaadminPages/admin-index/admin-index.component";
 
 const routes: Routes = [
   { path: '', component: IndexComponent, pathMatch: 'full'},
   { path: 'user/confirm', component: ConfirmEmailComponent, data: { token: ':token' }},
   { path: 'user/generateNewPassword', component: NewPasswordComponent, data: { token: ':token' }},
-  { path: 'admin', loadChildren: () => import('./aaapages/admin/admin.module').then(m => m.AdminModule), canActivate: [AdminGuard]},
+  { path: 'admin', component: AdminIndexComponent, pathMatch: 'full'},
+  // { path: 'admin', loadChildren: () => import('./aaapages/admin/admin.module').then(m => m.AdminModule), canActivate: [AdminGuard]},
   { path: '**', component: IndexComponent },
 ];
 
